7dea6ce51653649bcfb6629af44d6cc68c2360a1
Yuniarrr <5027211025@mhs.its.ac.id>
2023-12-28 02:25:13 +0700

    feat: next and prev btn
    feat: hide and show btn

--- practical-test/main.js
+++ practical-test/main.js
@@ -1,20 +1,18 @@
-// Fetch data from data.json
 fetch('data.json')
     .then(response => response.json())
     .then(data => renderProducts(data))
     .catch(error => console.error('Error fetching data:', error));
 
-// Function to render products
-function renderProducts(products) {
-    const productContainer = document.getElementById('productContainer');
+const productContainer = document.getElementById('product-container');
 
+function renderProducts(products) {
     products.forEach(product => {
         const card = document.createElement('div');
         card.classList.add('card');
 
         const image = document.createElement('img');
         image.src = product.image;
-        image.alt = '';
+        image.alt = product.name;
         image.srcset = '';
 
         const title = document.createElement('p');
@@ -23,17 +21,21 @@ function renderProducts(products) {
 
         const price = document.createElement('p');
         price.classList.add('price');
-        price.textContent = `Rp${product.price}`;
+        price.textContent = `Rp${product.price - (product.price * product.discount / 100)}`;
 
         const detailPrice = document.createElement('div');
         detailPrice.classList.add('detail-price');
-        const rp = document.createElement('p');
-        rp.textContent = 'Rp';
-        const disc = document.createElement('p');
-        disc.textContent = `disc ${product.discount}%`;
 
+        const rp = document.createElement('p');
+        rp.textContent = `Rp${product.price}`;
         detailPrice.appendChild(rp);
-        detailPrice.appendChild(disc);
+
+        if (product.discount != 0) {
+            const disc = document.createElement('p');
+            disc.textContent = `${product.discount}%`;
+            detailPrice.appendChild(disc);
+        }
+
 
         const btnBag = document.createElement('button');
         btnBag.classList.add('btn-bag');
@@ -48,3 +50,44 @@ function renderProducts(products) {
         productContainer.appendChild(card);
     });
 }
+
+// scroll to top
+const scrollToTop = document.getElementById('top-btn');
+scrollToTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+// scroll to next product item 30px
+const scrollToNext = document.getElementById('next-product');
+scrollToNext.addEventListener('click', () => {
+    productContainer.scrollBy({
+        left: 30,
+        behavior: 'smooth'
+    });
+});
+
+// scroll to prev product item 30px
+const scrollToPrev = document.getElementById('prev-product');
+scrollToPrev.addEventListener('click', () => {
+    productContainer.scrollBy({
+        left: -30,
+        behavior: 'smooth'
+    });
+});
+
+// hide details footer
+const hideButton = document.getElementById('hide-details');
+const loremText = document.querySelector('#footer p');
+hideButton.addEventListener('click', () => {
+    loremText.classList.toggle('hide');
+    if (loremText.classList.contains('hide')) {
+        hideButton.textContent = 'Show details';
+        loremText.style.display = 'none';
+    } else {
+        hideButton.textContent = 'Hide details';
+        loremText.style.display = 'block';
+    }
+});
